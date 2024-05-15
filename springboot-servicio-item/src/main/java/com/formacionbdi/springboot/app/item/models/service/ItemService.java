9ff41c55e39620e142c5480746e8b4aa60fbf812
claudioriquelmer <claudio.riquelme@outlook.es>
2024-05-14 21:43:46 -0400

    Primer Commit MS Item que se comunica con MS Producto

---
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/models/service/ItemService.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.item.models.service;
+
+import java.util.List;
+
+import com.formacionbdi.springboot.app.item.models.Item;
+
+public interface ItemService {
+
+	public List<Item> finAll();
+
+	public Item findById(Long id, Integer cantidad);
+
+}
