7ae1045603860f87991a3b82fb923214cd6d9e76
claudioriquelmer <claudio.riquelme@outlook.es>
2024-05-21 18:10:25 -0400

    Integrando Tolerancia de Fallos y Latencia con Hydrix

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controller/ItemController.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controller/ItemController.java
@@ -9,23 +9,39 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.springboot.app.item.models.Item;
+import com.formacionbdi.springboot.app.item.models.Producto;
 import com.formacionbdi.springboot.app.item.models.service.ItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class ItemController {
-	
+
 	@Autowired
 	@Qualifier("serviceFeign")
 	private ItemService itemService;
-	
+
 	@GetMapping("/listar")
 	public List<Item> listar() {
 		return itemService.finAll();
 	}
-	
+
+	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findById(id, cantidad);
 	}
-	
+
+	public Item metodoAlternativo(Long id, Integer cantidad) {
+		Item item = new Item();
+		Producto producto = new Producto();
+
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Camara Sony");
+		producto.setPrecio(500.00);
+		item.setProducto(producto);
+		return item;
+
+	}
+
 }
