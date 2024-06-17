7de5847fc29246fbf0f7e4e727131cd50fbdceb8
ClaudioRiquelmer <claudio.riquelmer@outlook.es>
2024-06-17 15:59:01 -0400

    Implementando handler CRUD en controlador de servicio items

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controller/ItemController.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controller/ItemController.java
@@ -15,10 +15,15 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.springboot.app.item.models.Item;
@@ -40,9 +45,9 @@ public class ItemController {
 	private CircuitBreakerFactory cbFactory;
 
 	@Autowired
-	@Qualifier("serviceFeign")
+	@Qualifier("serviceRestTemplate")
 	private ItemService itemService;
-	
+
 	@Value("${configuracion.texto}")
 	private String texto;
 
@@ -59,17 +64,17 @@ public class ItemController {
 		return cbFactory.create("items").run(() -> itemService.findById(id, cantidad),
 				e -> metodoAlternativo(id, cantidad, e));
 	}
-	
-	@CircuitBreaker(name="items", fallbackMethod = "metodoAlternativo")
+
+	@CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver2/{id}/cantidad/{cantidad}")
 	public Item detalle2(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findById(id, cantidad);
 	}
-	
-	@CircuitBreaker(name="items", fallbackMethod = "metodoAlternativo")
-	@TimeLimiter(name="items")
+
+	@CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo")
+	@TimeLimiter(name = "items")
 	@GetMapping("/ver3/{id}/cantidad/{cantidad}")
-	public CompletableFuture<Item>  detalle3(@PathVariable Long id, @PathVariable Integer cantidad) {
+	public CompletableFuture<Item> detalle3(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return CompletableFuture.supplyAsync(() -> itemService.findById(id, cantidad));
 	}
 
@@ -86,8 +91,8 @@ public class ItemController {
 		return item;
 
 	}
-	
-	//Método Alternativo para TimeLimiter
+
+	// Método Alternativo para TimeLimiter
 	public CompletableFuture<Item> metodoAlternativo2(Long id, Integer cantidad, Throwable e) {
 		logger.info(e.getMessage());
 		Item item = new Item();
@@ -101,20 +106,38 @@ public class ItemController {
 		return CompletableFuture.supplyAsync(() -> item);
 
 	}
-	
+
 	@GetMapping("/obtener-config")
 	public ResponseEntity<?> obtenerConfig(@Value("$server.port") String puerto) {
 		logger.info(texto);
 		Map<String, String> json = new HashMap<>();
 		json.put("texto", texto);
 		json.put("puerto", puerto);
-		
-		if(env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("dev")) {
+
+		if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
 			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
 			json.put("autor.email", env.getProperty("configuracion.autor.email"));
 		}
-		
+
 		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+	}
+
+	@PostMapping("/crear")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto crear(@RequestBody Producto producto) {
+		return itemService.save(producto);
+	}
+
+	@PutMapping("/editar/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto editar(@RequestBody Producto producto, @PathVariable Long id) {
+		return itemService.update(producto, id);
+	}
+
+	@DeleteMapping("/eliminar/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void eliminar(@PathVariable Long id) {
+		itemService.delete(id);
 	}
 
 }
